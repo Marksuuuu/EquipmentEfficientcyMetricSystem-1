5a79b69668318ac83a65e0d02b89d481a875c935
Alex Fernan Mercado <99195336+MeixieM@users.noreply.github.com>
2023-08-29 13:45:11 +0800

    Initial Commit

--- logs_data.py
+++ logs_data.py
@@ -32,6 +32,7 @@ class LogsData:
                 running_qty = int(item['running_qty'])
                 total_running_qty += running_qty
 
+        print("Total Running Quantity:", total_running_qty)
         return total_running_qty
         
     def total_remaining_qty(self):
@@ -57,6 +58,7 @@ class LogsData:
                 total_finished = int(item['total_finished'])
                 total_remaining_qty += total_finished
 
+        print("Total Remaining Quantity:", total_remaining_qty)
         return total_remaining_qty
 
 if __name__ == "__main__":
